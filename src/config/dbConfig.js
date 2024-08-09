a5350f49a2adc2c6e5e1f4dfc57b05dd1e559ea4
HP <awadhrajpatel95@gmail.com>
2024-08-09 17:15:36 +0530

    Express-starter

---
+++ src/config/dbConfig.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const serverConfig = require("./serverConfig");
+
+/***
+ * The below function helps us to connect to a mongodb server
+ */
+
+async function connectDB(){
+    try{
+        await mongoose.connect(serverConfig.DB_URL);
+        console.log("Successfully connected to the mongo db server.....");
+    }catch(error) {
+      console.log("Not able to mongoose.connect db server");
+      console.log(error);
+    }
+}
+
+
+
+module.exports = connectDB;
---
+++ src/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+// const bodyParse = require('body-parser');
+
+const serverConfig = require("./config/serverConfig");
+const connectDB = require("./config/dbConfig");
+
+const app = express();
+
+/***
+ * midilwere
+ */
+// app.use(bodyParse.json());
+// app.use(bodyParse.text());
+// app.use(bodyParse.urlencoded());
+
+/**   second methood use deserieser  */
+
+app.use(express.json());
+app.use(express.text());
+app.use(express.urlencoded({extended:true}));
+
+
+/*** */
+
+app.post('/ping',(req,res) =>{
+    console.log(req.body);
+    return res.json({message: "pong"});
+})
+
+app.listen(serverConfig.PORT, async () => {
+  await connectDB();
+  console.log(`Server started at port ${serverConfig.PORT}...!!`);
+});
+
+//Password: sufyaba19CMQ2HFC
+//Name: awadhrajpatel95
